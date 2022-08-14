eef6f3a2f400fd5cfbac52f9046a2c2d031421ea
xuyennt <xuyennt@127.0.0.200>
2022-08-14 09:52:54 +0700

    and_or

---
+++ selenium/javaTester/Topic_01_And_Or.java
@@ -0,0 +1,15 @@
+package javaTester;
+
+public class Topic_01_And_Or {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        boolean firstCondition;
+        boolean secondCondition;
+        
+        firstCondition = true;
+        secondCondition = false;
+        System.out.print(firstCondition && secondCondition);
+	}
+
+}
--- selenium/webdriver/Topic_04_Xpath.java
+++ selenium/webdriver/Topic_04_Xpath.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-public class Topic_00_template {
+public class Topic_04_Xpath {
 	
 //khaib báo biến đại diện cho thư viện selenium webdriver	
 WebDriver driver;
@@ -23,34 +23,34 @@ driver.manage().window().maximize();
 
 
 }
-
-public void TC_01_() {
+@Test
+public void TC_01() {
 	// bước 2 nhập vào url 
 	driver.get("http://live.techpanda.org/index.php/customer/account/login/");
 	
-	//<input class="is_required validate account_input form-control" 
-	//data-validate="isEmail" 
-   // type="text" id="email" 
-    //name="email" value="">
+//	<input type="email" 
+//	autocapitalize="off"
+//	autocorrect="off" 
+//	spellcheck="false" 
+//	name="login[username]" 
+//	value="" id="email" 
+//	class="input-text required-entry validate-email" title="Email Address">
 	//xpath
-	//input[@id='email']
-	//input[@name='email']
-	
-	// bước 3 click vào my  account để mở trang login lên
-	// tìm 1 element 
+	//input[@name='login[username]']
 	driver.findElement(By.id("email"));
-	driver.findElements(By.tagName("a"));
-	//Link text   --text tuyet doi
-	driver.findElement(By.linkText("New products"));
-	
-	
+	//new-users
+	driver.findElement(By.className("new-users"));
+	driver.findElement(By.name("login[username]"));
+	// Tagname tìm xem có bao nhiêu element/page 
+    driver.findElement(By.tagName("a"));
+
+
 }
 
 
 @Test
 public void TC_02_() {
-
-	}
+}
 @Test
 public void TC_03() {
 
